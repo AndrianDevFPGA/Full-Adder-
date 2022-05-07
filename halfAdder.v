7ea1fb4f6a5a64c2344b93a703c2a4d522f86c94
AndrianDevFPGA <104864333+AndrianDevFPGA@users.noreply.github.com>
2022-05-07 19:56:09 +0200

    Update halfAdder.v

--- halfAdder.v
+++ halfAdder.v
@@ -1,1 +1,22 @@
+/*
+  Author : Rakotojaona Nambinina
+  email : Andrianoelisoa.Rakotojaona@gmail.com
+  Description : Half adder code , used in the full adder 
+*/
+module halfAdder (
+       a1,
+       b1,
+       sum1,
+       carry1
+       );
+// input ports       
+input a1;
+input b1;
+// output ports
+output sum1;
+output carry1;
 
+assign sum1 = a1 ^ b1;
+assign carry = a1 & b1;
+
+endmodule
