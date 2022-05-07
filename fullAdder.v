f4e85434ccc998cb798c5a79ccc236007401ab3a
AndrianDevFPGA <104864333+AndrianDevFPGA@users.noreply.github.com>
2022-05-07 19:57:16 +0200

    Update fullAdder.v

--- fullAdder.v
+++ fullAdder.v
@@ -47,19 +47,4 @@ module fullAdder(
                .or2 (carry)
                );
 endmodule
-
-module orGate (
-              a2,
-              b2,
-              or2
-              );
-   // input port 
-   input a2;
-   input b2;
-   // output port 
-   output or2;
-   
-   assign or2 = a2 | b2;
- 
-endmodule
    
