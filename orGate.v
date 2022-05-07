af1ff44c450253306d77821d3323a74561e74230
AndrianDevFPGA <104864333+AndrianDevFPGA@users.noreply.github.com>
2022-05-07 19:57:46 +0200

    Update orGate.v

--- orGate.v
+++ orGate.v
@@ -1,1 +1,15 @@
 
+module orGate (
+              a2,
+              b2,
+              or2
+              );
+   // input port 
+   input a2;
+   input b2;
+   // output port 
+   output or2;
+   
+   assign or2 = a2 | b2;
+ 
+endmodule
