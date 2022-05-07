b81211d0e9d2bf2ec4e1ac7b312ab0ee51b1893d
AndrianDevFPGA <104864333+AndrianDevFPGA@users.noreply.github.com>
2022-05-07 19:54:45 +0200

    Update fullAdder.v

--- fullAdder.v
+++ fullAdder.v
@@ -48,25 +48,6 @@ module fullAdder(
                );
 endmodule
 
-
-module halfAdder (
-       a1,
-       b1,
-       sum1,
-       carry1
-       );
-// input ports       
-input a1;
-input b1;
-// output ports
-output sum1;
-output carry1;
-
-assign sum1 = a1 ^ b1;
-assign carry = a1 & b1;
-
-endmodule
-
 module orGate (
               a2,
               b2,
